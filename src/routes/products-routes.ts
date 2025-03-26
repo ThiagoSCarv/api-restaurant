0a2986f31a7e6aa391c9e3979a0e4d6074c9e1f2
Thiago Soares <thiagocarv4@gmail.com>
2025-03-26 20:16:51 -0300

    Criando o Middleware de Tratamento de Exceções

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -2,7 +2,7 @@ import { ProductController } from "@/controllers/products-controller";
 import { Router } from "express";
 
 const productsRoutes = Router();
-const productsController = new ProductController()
+const productsController = new ProductController();
 
 productsRoutes.get("/", productsController.index);
 
