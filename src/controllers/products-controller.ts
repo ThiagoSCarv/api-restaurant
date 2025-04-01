527ed2477dfa30f451a63e18d2b28e6c4da2aba1
Thiago Soares <thiagocarv4@gmail.com>
2025-04-01 19:24:58 -0300

    validando dados do produto

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,10 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "@/utils/AppError";
+import { string, z } from "zod";
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
       return response.json({ message: "OK" });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string({ required_error: "name is required!" }).trim().min(6),
+        price: z.number().gt(0, { message: "value must to be greater than 0"})
+      });
+
+      const {name, price} = bodySchema.parse(request.body)
+
+      return response.status(201).json({ name, price });
     } catch (error) {
       next(error);
     }
