e86a4c929c4afa4880166389265ca260fe899a0d
Thiago Soares <thiagocarv4@gmail.com>
2025-04-02 19:33:00 -0300

    abrindo a mesa

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,11 +1,24 @@
 import { Request, Response, NextFunction } from "express";
+import { knex } from "@/database/knex";
+import { z } from "zod";
 
 class TablesSessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
-      return response.status(201).json()
+      const bodySchema = z.object({
+        table_id: z.number(),
+      })
+
+      const { table_id } = bodySchema.parse(request.body)
+
+      await knex<TablesSessionsRepository>("table_sessions").insert({
+        table_id,
+        opened_at: knex.fn.now()
+      })
+
+      return response.status(201).json();
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 }
