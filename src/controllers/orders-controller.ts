5219cba0258562048d9aa0cd901b1c008d3f1a03
Thiago Soares <thiagocarv4@gmail.com>
2025-04-03 16:08:30 -0300

    Listando pedidos por mesa

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -40,7 +40,7 @@ class OrdersController {
         table_session_id,
         product_id,
         quantity,
-        price: product.price
+        price: product.price,
       });
 
       return response.status(201).json();
@@ -51,9 +51,23 @@ class OrdersController {
 
   async index(request: Request, response: Response, next: NextFunction) {
     try {
-      return response.json()
+      const { table_session_id } = request.params;
+
+      const order = await knex("orders")
+        .select(
+          "orders.id",
+          "orders.table_session_id",
+          "orders.product_id",
+          "products.name",
+          "orders.price",
+          "orders.quantity"
+        )
+        .join("products", "products.id", "orders.product_id")
+        .where({ table_session_id });
+
+      return response.json(order);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 }
