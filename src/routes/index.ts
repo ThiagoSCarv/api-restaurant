b20d34002d19bb5f6e84c1e2a610baa194fc80d8
Thiago Soares <thiagocarv4@gmail.com>
2025-04-03 15:25:09 -0300

    Tratando Dados do Pedido

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,12 @@ import { Router } from "express";
 import { tableSessionsRoutes } from "./tables-sessions-routes";
 import { productsRoutes } from "./products-routes";
 import { tableRoutes } from "./tables-routes";
+import { ordersRoutes } from "./orders-routes";
 
 const routes = Router();
 routes.use("/products", productsRoutes);
 routes.use("/tables", tableRoutes)
 routes.use("/tables-sessions", tableSessionsRoutes)
+routes.use("/orders", ordersRoutes)
 
 export { routes };
